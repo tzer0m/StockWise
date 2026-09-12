892b99529476595d1686a44065893e85f8c94926
Tom Oddy <tom_oddy@live.co.uk>
2026-09-12 11:48:52 +0100

    Added oidc

--- StockWise/Program.cs
+++ StockWise/Program.cs
@@ -1,10 +1,31 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.EntityFrameworkCore;
 using StockWise.Data;
 
 // Create web application builder.
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
-builder.Services.AddRazorPages();
+builder.Services.AddRazorPages(options => options.Conventions.AuthorizeFolder("/").AllowAnonymousToPage("/Error"));
 builder.Services.AddDbContext<StockWiseDbContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("StockWiseDb")));
+
+// Configure authentication against the homelab's OIDC provider (Pocket ID).
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
+}).AddCookie().AddOpenIdConnect(options =>
+{
+    options.Authority = builder.Configuration["Oidc:Authority"];
+    options.ClientId = builder.Configuration["Oidc:ClientId"];
+    options.ClientSecret = builder.Configuration["Oidc:ClientSecret"];
+    options.ResponseType = "code";
+    options.UsePkce = true;
+    options.SaveTokens = true;
+    options.Scope.Clear();
+    options.Scope.Add("openid");
+    options.Scope.Add("profile");
+    options.Scope.Add("email");
+});
 
 // Create the web application and configure.
 WebApplication app = builder.Build();
@@ -15,6 +36,7 @@ if (!app.Environment.IsDevelopment())
 }
 app.UseHttpsRedirection();
 app.UseRouting();
+app.UseAuthentication();
 app.UseAuthorization();
 app.MapStaticAssets();
 app.MapRazorPages().WithStaticAssets();
