08914aeeced6e7a312e2d0b6b45cba5086be788a
Tom Oddy <tom_oddy@live.co.uk>
2026-09-12 11:06:44 +0100

    Added migrations for default locations

---
+++ StockWise/Migrations/20260912100625_SeedCategoriesAndLocations.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace StockWise.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedCategoriesAndLocations : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "StorageCategories",
+                columns: new[] { "CategoryId", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Fridge" },
+                    { 2, "Freezer" },
+                    { 3, "Pantry" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Locations",
+                columns: new[] { "LocationId", "CategoryId", "Name" },
+                values: new object[,]
+                {
+                    { 1, 1, "Fridge Door" },
+                    { 2, 1, "Fridge Shelves" },
+                    { 3, 2, "Inside Freezer" },
+                    { 4, 2, "Outside Freezer" },
+                    { 5, 3, "Drawers" },
+                    { 6, 3, "Cereal Cupboard" },
+                    { 7, 3, "Cupboard L" },
+                    { 8, 3, "Cupboard R" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Locations",
+                keyColumn: "LocationId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Locations",
+                keyColumn: "LocationId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Locations",
+                keyColumn: "LocationId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Locations",
+                keyColumn: "LocationId",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Locations",
+                keyColumn: "LocationId",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Locations",
+                keyColumn: "LocationId",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Locations",
+                keyColumn: "LocationId",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "Locations",
+                keyColumn: "LocationId",
+                keyValue: 8);
+
+            migrationBuilder.DeleteData(
+                table: "StorageCategories",
+                keyColumn: "CategoryId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "StorageCategories",
+                keyColumn: "CategoryId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "StorageCategories",
+                keyColumn: "CategoryId",
+                keyValue: 3);
+        }
+    }
+}
